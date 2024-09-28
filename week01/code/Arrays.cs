2814db79be102642eda211d5179906634d4232bc
keviner99 <90742482+keviner99@users.noreply.github.com>
2024-09-28 11:35:02 -0500

    include methods documentation

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -44,5 +44,10 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+
+        // Use the following documentation https://learn.microsoft.com/en-us/dotnet/api/system.collections.generic.list-1?view=net-6.0#methods 
+        // to find the methods using for this part (AddRange, GetRange, RemoveRange, InsertRange)
+
+        
     }
 }
