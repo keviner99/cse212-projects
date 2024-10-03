096c3dd1ca2fc35fcd6968486eba042770c70f12
keviner99 <90742482+keviner99@users.noreply.github.com>
2024-10-03 00:04:32 -0500

    add tests for testpriorityQueue2

--- week02/code/PriorityQueue_Tests.cs
+++ week02/code/PriorityQueue_Tests.cs
@@ -16,13 +16,19 @@ public class PriorityQueueTests
     }
 
     [TestMethod]
-    // Scenario: 
-    // Expected Result: 
-    // Defect(s) Found: 
+    // Scenario: Enqueu multiple items with same priority
+    // Expected Result: The first item added should be dequeued first
+    // Defect(s) Found: it returned second at first
     public void TestPriorityQueue_2()
     {
         var priorityQueue = new PriorityQueue();
-        Assert.Fail("Implement the test case and then remove this.");
+        priorityQueue.Enqueue("first", 10);
+        priorityQueue.Enqueue("second", 10);
+        priorityQueue.Enqueue("third", 5);
+
+        Assert.AreEqual("first", priorityQueue.Dequeue());
+        Assert.AreEqual("second", priorityQueue.Dequeue());
+        Assert.AreEqual("third", priorityQueue.Dequeue());
     }
 
     // Add more test cases as needed below.
